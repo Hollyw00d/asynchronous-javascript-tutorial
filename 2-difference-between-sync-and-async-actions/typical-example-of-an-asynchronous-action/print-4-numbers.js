82b1da09bf408edca2d43810ecd22e3226a13ed8
Matt Jennings <matt@mattjennings.net>
2020-09-22 15:39:49 -0700

    added comments for example asynchronous code

--- 2-difference-between-sync-and-async-actions/typical-example-of-an-asynchronous-action/print-4-numbers.js
+++ 2-difference-between-sync-and-async-actions/typical-example-of-an-asynchronous-action/print-4-numbers.js
@@ -8,6 +8,7 @@ function print2() {
     function getNumber2() {
         return 2;
     }
+    // Blocking function call below
     const number2 = getNumber2();
     console.log(number2);
 }
@@ -15,6 +16,20 @@ function print2() {
 
 function print3() {
     const fs = require('fs');
+    /*
+    1.
+    Reading data from a file is a non-blocking call, 
+    which is shown below
+    in the "number3" parameter below which 
+    refers to the "number3.txt" file text, from the fs.readFile() method,
+    and is an asynchronous functiona and passed into a callback function.
+
+    2. The callback function below is an asynchronous function and is executed
+    after all the synchronous, blocking function calls such as:
+    print1();
+    print2();
+    print4();
+    */
     fs.readFile('./number3.txt', 'utf-8', function (err, number3) {
         console.log(number3);
     });
