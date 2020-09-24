1038b2fa9d7f2f779e28092a43100d12971d2651
Matt Jennings <matt@mattjennings.net>
2020-09-23 17:10:50 -0700

    set up mocha and chai test

---
+++ testing-callbacks/test/calculate-square.test.js
@@ -0,0 +1,11 @@
+import calculateSquare from '../calculate-square.js';
+import pkg from 'chai';
+const {expect} = pkg;
+
+describe('calculateSquare', function() {
+  it('should return 4 if passed 2', function() {
+    calculateSquare(2, function(error, result) {
+      expect(result).to.equal(4);
+    });
+  });
+});
